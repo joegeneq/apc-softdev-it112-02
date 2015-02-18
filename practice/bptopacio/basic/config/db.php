82c7164e31b77ce0864bb30b42bcd71993d84fe4
Bernard Dominic P Topacio <bptopacio@gmail.com>
2015-02-18 08:51:03 +0800

    edit db file on practice/bptopacio/basic/config/db.php

--- practice/bptopacio/basic/config/db.php
+++ practice/bptopacio/basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=it122_exercise1',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
