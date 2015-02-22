ad567150523aa6502d1ee15bc80dbf9629ebfff8
Bernard Dominic P Topacio <bptopacio@gmail.com>
2015-02-22 15:00:49 +0800

    update bptopacio folder

---
+++ practice/bptopacio/basic/models/Region.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "region".
+ *
+ * @property integer $id
+ * @property string $region.code
+ * @property string $region.description
+ *
+ * @property Province[] $provinces
+ */
+class Region extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'region';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['region.code', 'region.description'], 'string', 'max' => 32]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'region.code' => 'Region Code',
+            'region.description' => 'Region Description',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProvinces()
+    {
+        return $this->hasMany(Province::className(), ['region_id' => 'id']);
+    }
+}
