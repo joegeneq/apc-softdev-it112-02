cf30e804853f67152e3a8fab3e8d5873d3f068b6
Bernard Dominic P Topacio <bptopacio@gmail.com>
2015-02-22 12:18:32 +0800

    update basic folder under bptopacio folder.

--- practice/bptopacio/basic/config/db.php
+++ practice/bptopacio/basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=it122_exercise1',
+    'dsn' => 'mysql:host=localhost;dbname=it112_exercise1',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
