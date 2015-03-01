e9f834cb6ab9643b5db41a43adb723a43711f3c6
Paolo Lansigan <kemchii.69@gmail.com>
2015-03-01 17:55:39 +0800

    Insert one record for address and insert one record for comment

--- practice/midterm_test/Lansigan_Paolo_Luis/ourspace/views/layouts/main.php
+++ practice/midterm_test/Lansigan_Paolo_Luis/ourspace/views/layouts/main.php
@@ -38,8 +38,9 @@ AppAsset::register($this);
                     ['label' => 'Home', 'url' => ['/site/index']],
                     ['label' => 'About', 'url' => ['/site/about']],
                     ['label' => 'Contact', 'url' => ['/site/contact']],
-                    ['label' => 'myaddress', 'url' => ['/myaddress/index']],
-                    ['label' => 'mycomment', 'url' => ['/mycomment/index']],
+                    ['label' => 'Myaddress', 'url' => ['/myaddress/index']],
+                    ['label' => 'Mycomment', 'url' => ['/mycomment/index']],
+                    
                     Yii::$app->user->isGuest ?
                         ['label' => 'Login', 'url' => ['/site/login']] :
                         ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
