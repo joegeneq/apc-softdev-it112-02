c4cc645e1f908e6dc10da9d4f0158c9f860f3144
Bernard Dominic P Topacio <bptopacio@gmail.com>
2015-03-04 23:47:01 +0800

    generated crud for postion,user, employee, employee_has_postion

---
+++ application/bir_dwts/backend/controllers/EmployeeHasPositionController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use backend\models\EmployeeHasPosition;
+use backend\models\EmployeeHasPositionSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * EmployeeHasPositionController implements the CRUD actions for EmployeeHasPosition model.
+ */
+class EmployeeHasPositionController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all EmployeeHasPosition models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new EmployeeHasPositionSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single EmployeeHasPosition model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new EmployeeHasPosition model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new EmployeeHasPosition();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing EmployeeHasPosition model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing EmployeeHasPosition model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the EmployeeHasPosition model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return EmployeeHasPosition the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = EmployeeHasPosition::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
--- application/bir_dwts/backend/views/position/_search.php
+++ application/bir_dwts/backend/views/position/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model backend\models\PositionSearch */
+/* @var $model backend\models\PostionSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
