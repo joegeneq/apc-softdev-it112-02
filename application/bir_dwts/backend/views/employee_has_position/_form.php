35bcbf2ac42f676dcef46282b81ee9d1a812b0bb
Bernard Dominic P Topacio <bptopacio@gmail.com>
2015-03-04 21:39:17 +0800

    update files backend/runtime/runtime

--- application/bir_dwts/backend/views/employee_has_position/_form.php
+++ application/bir_dwts/backend/views/employee_has_position/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use backend\models\Employee;
+use backend\models\Postion;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\EmployeeHasPosition */
@@ -12,9 +15,15 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'employee_id')->textInput() ?>
+    <?= $form->field($model, 'employee_id')->dropDownList(
+    ArrayHelper::map(Employee::find()->all(),'id','name'),
+        ['prompt'=>'Select myaddress']
+    ) ?>
 
-    <?= $form->field($model, 'position_id')->textInput() ?>
+    <?= $form->field($model, 'position_id')->dropDownList(
+    ArrayHelper::map(Postion::find()->all(),'id','postion_description'),
+        ['prompt'=>'Select myaddress']
+    ) ?>
 
     <?= $form->field($model, 'employee_position_start_date')->textInput() ?>
 
