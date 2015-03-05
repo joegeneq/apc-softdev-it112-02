944eda8660dfc7bc86081d0c2881c1534373dc96
Bernard Dominic P Topacio <bptopacio@gmail.com>
2015-03-05 18:52:40 +0800

    remove .gitignore for params-local

---
+++ application/bir_dwts/backend/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'LEQ_uv2O3UGDw8P8ush06nfAC3-zn307',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
