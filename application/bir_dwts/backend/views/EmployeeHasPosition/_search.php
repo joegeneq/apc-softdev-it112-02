6d93a4bc8e4401de6ce52939fb82ab77b12f4023
Bernard Dominic P Topacio <bptopacio@gmail.com>
2015-03-05 09:57:51 +0800

    added bir_dwts under application folder, model and Crud generated for user, employee, employeehasposition, position and made foreign to dropddownlist

---
+++ application/bir_dwts/backend/views/EmployeeHasPosition/_search.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\EmployeeHasPositionSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="employee-has-position-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'employee_id') ?>
+
+    <?= $form->field($model, 'position_id') ?>
+
+    <?= $form->field($model, 'employee_position_start_date') ?>
+
+    <?= $form->field($model, 'employee_position_end_date') ?>
+
+    <?php // echo $form->field($model, 'create_time') ?>
+
+    <?php // echo $form->field($model, 'update_time') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
