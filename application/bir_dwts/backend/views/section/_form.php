733aebc08bf53d467a69be08050b2fb1612606c5
Mark Rianzares <markrianzares@gmail.com>
2015-03-27 00:34:37 +0800

    Generated Model and CRUD in backend for station desk, section, user, station desk role, position, employee, document_category and document_priority

---
+++ application/bir_dwts/backend/views/section/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Section */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="section-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'section_code')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'section_name')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'section_description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'create_time')->textInput() ?>
+
+    <?= $form->field($model, 'update_time')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
