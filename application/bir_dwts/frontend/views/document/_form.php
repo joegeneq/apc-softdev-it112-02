b8fadcc52eb5faa92aebe6b9d94d01f04cfa6abc
Bernard Dominic P Topacio <bptopacio@gmail.com>
2015-03-27 04:01:51 +0800

    Update UI

--- application/bir_dwts/frontend/views/document/_form.php
+++ application/bir_dwts/frontend/views/document/_form.php
@@ -2,6 +2,12 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use backend\models\DocumentPriority;
+use backend\models\Employee;
+use frontend\models\Customer;
+use frontend\models\CompanyAgency;
+
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Document */
@@ -22,17 +28,31 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'document_category')->textInput() ?>
 
-    <?= $form->field($model, 'document_priority_id')->textInput() ?>
+    <?= $form->field($model, 'document_priority_id')->dropDownList(
+        ArrayHelper::map(DocumentPriority::find()->all(),'id','document_priority_name'),
+        ['prompt'=>'Select Document Priority']
+    ) ?>
 
     <?= $form->field($model, 'document_type')->textInput(['maxlength' => 45]) ?>
 
     <?= $form->field($model, 'document_comment')->textInput(['maxlength' => 45]) ?>
 
-    <?= $form->field($model, 'employee_id')->textInput() ?>
+    <?= $form->field($model, 'employee_id')->dropDownList(
+        ArrayHelper::map(Employee::find()->all(),'id','last_name'),
+        ['prompt'=>'Select Employee']
+    ) ?>
 
-    <?= $form->field($model, 'customer_id')->textInput() ?>
+    <?= $form->field($model, 'customer_id')->dropDownList(
+        ArrayHelper::map(Customer::find()->all(),'id','customer_lastname'),
+        ['prompt'=>'Select Customer']
+    ) ?>
 
     <?= $form->field($model, 'company_agency_id')->textInput() ?>
+
+    <?= $form->field($model, 'company_agency_id')->dropDownList(
+        ArrayHelper::map(CompanyAgency::find()->all(),'id','company_agencyl_name'),
+        ['prompt'=>'Select CompanyAgency']
+    ) ?>
 
     <?= $form->field($model, 'document_image_front_page')->textInput() ?>
 
