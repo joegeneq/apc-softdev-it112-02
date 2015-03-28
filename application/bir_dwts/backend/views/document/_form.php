d275de2e7b2f0576205811adf3169b33f1689a99
Mark Rianzares <markrianzares@gmail.com>
2015-03-28 21:31:02 +0800

    Updated Document(dropDownList, view & update)

--- application/bir_dwts/backend/views/document/_form.php
+++ application/bir_dwts/backend/views/document/_form.php
@@ -2,6 +2,14 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use common\models\DocumentCategory;
+use common\models\DocumentPriority;
+use common\models\DocumentType;
+use common\models\Employee;
+use common\models\Customer;
+use common\models\CompanyAgency;
+use common\models\Section;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Document */
@@ -20,19 +28,37 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'document_target_date')->textInput() ?>
 
-    <?= $form->field($model, 'document_category')->textInput() ?>
+    <?= $form->field($model, 'document_category')->dropDownList(
+        ArrayHelper::map(DocumentCategory::find()->all(),'id', 'document_category_name'),
+        ['prompt'=>'Select Category']
+    ) ?>
 
-    <?= $form->field($model, 'document_priority_id')->textInput() ?>
+    <?= $form->field($model, 'document_priority_id')->dropDownList(
+        ArrayHelper::map(DocumentPriority::find()->all(),'id', 'document_priority_name'),
+        ['prompt'=>'Select Priority']
+    ) ?>
 
-    <?= $form->field($model, 'document_type_id')->textInput() ?>
-
+    <?= $form->field($model, 'document_type_id')->dropDownList(
+        ArrayHelper::map(DocumentType::find()->all(),'id', 'document_type_name'),
+        ['prompt'=>'Select Type']
+    ) ?>
+	
     <?= $form->field($model, 'document_comment')->textInput(['maxlength' => 45]) ?>
 
-    <?= $form->field($model, 'employee_id')->textInput() ?>
+    <?= $form->field($model, 'employee_id')->dropDownList(
+        ArrayHelper::map(Employee::find()->all(),'id', 'employee_last_name'),
+        ['prompt'=>'Select Employee']
+    ) ?>
 
-    <?= $form->field($model, 'customer_id')->textInput() ?>
+    <?= $form->field($model, 'customer_id')->dropDownList(
+        ArrayHelper::map(Customer::find()->all(),'id', 'customer_lastname'),
+        ['prompt'=>'Select Customer']
+    ) ?>
 
-    <?= $form->field($model, 'company_agency_id')->textInput() ?>
+    <?= $form->field($model, 'company_agency_id')->dropDownList(
+        ArrayHelper::map(CompanyAgency::find()->all(),'id', 'company_agency_name'),
+        ['prompt'=>'Select Company Agency']
+    ) ?>
 
     <?= $form->field($model, 'document_image_front_page')->textInput() ?>
 
@@ -40,7 +66,10 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'update_time')->textInput() ?>
 
-    <?= $form->field($model, 'section_id')->textInput() ?>
+    <?= $form->field($model, 'section_id')->dropDownList(
+        ArrayHelper::map(Section::find()->all(),'id', 'section_code'),
+        ['prompt'=>'Select Section']
+    ) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
--- application/bir_dwts/common/models/Document.php
+++ application/bir_dwts/common/models/Document.php
@@ -3,6 +3,8 @@
 namespace common\models;
 
 use Yii;
+use yii\db\ActiveRecord;
+use yii\db\Expression;
 
 /**
  * This is the model class for table "document".
@@ -145,4 +147,19 @@ class Document extends \yii\db\ActiveRecord
     {
         return $this->hasMany(DocumentWorkflow::className(), ['document_id' => 'id']);
     }
+	
+	public function behaviors()
+    {
+        return [
+            'timestamp' => [
+            'class' => 'yii\behaviors\TimestampBehavior',
+            'attributes' => [
+            ActiveRecord::EVENT_BEFORE_INSERT => ['create_time', 'update_time'],
+            ActiveRecord::EVENT_BEFORE_UPDATE => ['update_time'],
+            ],
+            'value' => new Expression('NOW()'),
+        ],
+        ];
+    }
+	
 }
